b09f2e67701a35c427595760b517789d1767e8a4
Carlinne <carli.pacheco95@gmail.com>
2016-11-11 13:37:05 -0600

    Actualizacion

--- alumnos/datos/views.py
+++ alumnos/datos/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def hola(request):
-    return HttpResponse("Hello world desde Django")
+    return render(request, "index.html", {"nombre": "Carlinne Pacheco"})
 # Create your views here.
+
+def formulario(request):
+    form = "Hola este es mi formulario"
+    return render(request, "formulario.html", {"formulario": form})
